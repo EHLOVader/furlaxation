2f21c8af9db5a59918eb05ea417701dbe39b67e8
Joseph Richardson <EHLOVader@users.noreply.github.com>
2024-06-28 23:10:12 -0400

    :construction: WIP add 404 page for routing

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     base: '/furlaxation/',
-    plugins: [react()]
+    plugins: [react()],
+    server: {
+        historyApiFallback: true,
+    },
  })
