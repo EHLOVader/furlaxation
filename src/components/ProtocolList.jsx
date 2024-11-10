7eb453c986d8fc7b2593fe417d887f165e3f7e38
Joseph Richardson <EHLOVader@users.noreply.github.com>
2024-11-09 20:11:32 -0500

    Make protocol buttons paws and add some description to this

--- src/components/ProtocolList.jsx
+++ src/components/ProtocolList.jsx
@@ -6,13 +6,17 @@ export default class ProtocolList extends React.Component{
     render() {
         return (
             <>
-                <h1>Protocols</h1>
-                <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                <h1>Furlaxation</h1>
+                <p>Welcome! Furlaxation is a web app to help you train your furry friend through the use of
+                relaxation protocols.
+                The relaxation protocol, based on <a href="https://web.archive.org/web/20240408134659/https://www.karenoverall.com/wp-content/uploads/2020/06/Protocol-for-relaxation_Overall.pdf" target="_blank">Karen L. Overall's research</a></p>
+                <h2>Protocols</h2>
+                <ul className="p-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-center">
                     {this.props.protocols.map((protocol, index) => (
                         <li key={index}
-                            className="rounded overflow-hidden shadow-lg p-6 bg-white border border-gray-300">
-                            <Link to={`/protocol/${index}`} className="text-blue-500 hover:text-blue-800">
-                                {protocol.name}
+                            className="protocol-button">
+                            <Link to={`/protocol/${index}`} className="text-gray-950" title={protocol.name}>
+                                {protocol.id}
                             </Link>
                         </li>
                     ))}
